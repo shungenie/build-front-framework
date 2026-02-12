33000a947778e035d340139f63a818fe69ba90ad
shungenie <ca02291@cartahd.com>
2026-02-12 21:30:21 +0900

    ✨ addEventListener 関数と addEventListeners 関数を実装し、イベントリスナーの追加機能を追加しました

---
+++ runtime/src/events.js
@@ -0,0 +1,15 @@
+export function addEventListner(eventName, handler, el) {
+    el.addEventListener(eventName, handler);
+    return handler;
+}
+
+export function addEventListeners(listeners = {}, el) {
+    const addedListeners = {};
+
+    Object.entries(listeners).forEach(([eventName, handler]) => {
+        const listener = addEventListener(eventName, handler, el);
+        addedListeners[eventName] = listener;
+    });
+
+    return addedListeners;
+}
