57b8ffcf8fc5a18bc201dad9644c286ac1637df1
shungenie <ca02291@cartahd.com>
2026-04-07 17:47:55 +0900

    ✨ feat: 配列とオブジェクトの差分を計算する関数を追加した

--- packages/runtime/src/utils/arrays.js
+++ packages/runtime/src/utils/arrays.js
@@ -1,3 +1,88 @@
+export const ARRAY_DIFF_OP = {
+    ADD: 'add',
+    REMOVE: 'remove',
+    MOVE: 'move',
+    NOOP: 'noop',
+}
+
+class ArrayWithOriginalIndices {
+    #array = [];
+    #originalIndices = [];
+    #equalsFn;
+
+    constructor(array, equalsFn) {
+        this.#array = [...array];
+        this.#originalIndices = array.map((_, i) => i);
+        this.#equalsFn = equalsFn;
+    }
+
+    get length() {
+        return this.#array.length;
+    }
+
+    isRemoval(index, newArray) {
+        if (index >= this.length) {
+            return false;
+        }
+
+        const item = this.#array[index];
+        const indexInNewArray = newArray.findIndex((newItem) => this.#equalsFn(item, newItem));
+
+        return indexInNewArray === -1;
+    }
+
+    removeItem(index) {
+        const operation = {
+            op: ARRAY_DIFF_OP.REMOVE,
+            index,
+            item: this.#array[index],
+        }
+
+        this.#array.splice(index, 1);
+        this.#originalIndices.splice(index, 1);
+
+        return operation;
+    }
+}
+
 export function withoutNulls(arr) {
     return arr.filter((item) => item != null);
+}
+
+export function arraysDiff(oldArray, newArray) {
+    return {
+        added: newArray.filter(
+            (newItem) => !oldArray.includes(newItem)
+        ),
+        removed: oldArray.filter(
+            (oldItem) => !newArray.includes(oldItem)
+        ),
+    }
+}
+
+export function arraysDiffSequence(
+    oldArray,
+    newArray,
+    equalsFn = (a, b) => a === b
+) {
+    const sequence = [];
+    const array = newArrayWithOriginalIndices(oldArray, equalsFn);
+
+    for (let index = 0; index < newArray.length; index++) {
+        if (array.isRemoval(index, newArray)) {
+            sequence.push(array.removeItem(index));
+            index--;
+            continue;
+        }
+
+ // TODO: noop case
+
+ // TODO: addition case
+
+ // TODO: move case
+    }
+
+ // TODO: remove extra items
+
+    return sequence
 }
