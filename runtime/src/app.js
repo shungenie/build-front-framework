d3d48e54a97da902588587f9f95ad3672672f007
shungenie <ca02291@cartahd.com>
2026-04-07 10:38:58 +0900

    ✨ createApp関数にreducerを追加し、状態管理機能を強化しました

--- runtime/src/app.js
+++ runtime/src/app.js
@@ -1,9 +1,22 @@
 import { destroyDOM } from './destroy-dom';
+import { Dispatcher } from './dispatcher';
 import { moundDOM } from './mound-dom';
 
-export function createApp({state, view}) {
+export function createApp({state, view, reducers = {}}) {
     let parentEl = null;
     let vdom = null;
+
+    const dispatcher = new Dispatcher();
+    const subscriptions = [dispatcher.afterEveryCommand(renderApp)];
+
+    for (const actionName in reducers) {
+        const reducer = reducers[actionName];
+
+        const subs = dispatcher.subscribe(actionName, (payload) => {
+            state = reducer(state, payload);
+        })
+        subscriptions.push(subs);
+    }
 
     function renderApp() {
         if (vdom) {
