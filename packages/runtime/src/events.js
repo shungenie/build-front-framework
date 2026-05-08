4ab2d5e97792a6ea13f51efabcd185606e417907
shungenie <ca02291@cartahd.com>
2026-05-08 10:16:54 +0900

    ✨ feat: mountDOM関数にhostComponent引数を追加し、子要素のマウント処理を修正した

--- packages/runtime/src/events.js
+++ packages/runtime/src/events.js
@@ -1,13 +1,34 @@
-export function addEventListener(eventName, handler, el) {
-    el.addEventListener(eventName, handler);
-    return handler;
+export function addEventListener(
+  eventName, 
+  handler, 
+  el,
+  hostComponent = null
+) {
+    function boundHandler() {
+      hostComponent
+        ? handler.apply(hostComponent, arguments)
+        : handler(...arguments);
+    }
+
+    el.addEventListener(eventName, boundHandler);
+
+    return boundHandler;
 }
 
-export function addEventListeners(listeners = {}, el) {
+export function addEventListeners(
+  listeners = {}, 
+  el,
+  hostComponent = null
+) {
     const addedListeners = {};
 
     Object.entries(listeners).forEach(([eventName, handler]) => {
-        const listener = addEventListener(eventName, handler, el);
+        const listener = addEventListener(
+          eventName, 
+          handler, 
+          el,
+          hostComponent
+        );
         addedListeners[eventName] = listener;
     });
 
