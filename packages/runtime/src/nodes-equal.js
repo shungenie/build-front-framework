2b74155882c9909d53113ea2d2a460b6af6ccb44
shungenie <ca02291@cartahd.com>
2026-05-17 22:09:29 +0900

    ✨ feat: areNodesEqual関数にCOMPONENTタイプのノード比較処理を追加した

--- packages/runtime/src/nodes-equal.js
+++ packages/runtime/src/nodes-equal.js
@@ -12,5 +12,12 @@ export function areNodesEqual(nodeOne, nodeTwo) {
         return tagOne === tagTwo;
     }
 
+    if (nodeOne.type === DOM_TYPES.COMPONENT) {
+        const { tag: componentOne, props: { key: keyOne } } = nodeOne;
+        const { tag: componentTwo, props: { key: keyTwo } } = nodeTwo;
+
+        return componentOne === componentTwo && keyOne === keyTwo;
+    }
+
     return true;
 }
